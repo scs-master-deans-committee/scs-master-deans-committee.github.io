1204b7d315114a419356d40c99d07789ea774ad1
Anant Kaushik <anantkaushik@Anants-MacBook-Pro.local>
2020-08-14 13:26:05 -0400

    Added minutes

---
+++ data/minutes.js
@@ -0,0 +1,87 @@
+minutes = [
+  {
+    "meeting-date": {
+      date: "27",
+      month: "Jun",
+      year: "2020",
+    },
+    minutes: {
+      main: ["Design contact form for SCS students to contact the committee"],
+      operational: [
+        "Floating scribe position for dean’s meeting",
+        "Constitution review and update",
+        "Meet with CPDC organizers in this week",
+        "First year slack (could be school or department level)",
+      ],
+      "Tasks for next meeting": [
+        "Improving communication flow with the university",
+        "Tackle Loneliness: Can be attacked from a class level (enforced groups) ",
+      ],
+    },
+  },
+  {
+    "meeting-date": {
+      date: "20",
+      month: "Jun",
+      year: "2020",
+    },
+    minutes: {
+      main: [
+        "Discussion on the meeting with CMU SPIRIT and Color Stack from Cornell by Daniel and Anant. Here are some topics discussed",
+        "Change of culture, increase black and Latin students and professors for diversity",
+        "Training for TA’s to eliminate racial bias in grading",
+        "Create a chapter of Color Stack at CMU, and get funding for travel and operation",
+        "Ask dean for the student diversity ratios",
+        {
+          "Topics assigned to members (For the meeting with Dean)": [
+            "Black lives matter: Daniel",
+            "International students: Lijun",
+            "Online accommodation: Neil",
+            "Constitution: Kiran",
+            "Mental health: Romel",
+            "TOC (tentative): Kiran",
+          ],
+        },
+      ],
+      "Tasks for next meeting": [
+        "Website To add Text for organization overview",
+        "Organize past meeting minutes with dean to publish",
+        "Meeting: resolve Zoom recording issue",
+      ],
+    },
+  },
+  {
+    "meeting-date": {
+      date: "13",
+      month: "Jun",
+      year: "2020",
+    },
+    minutes: {
+      main: [
+        "Updates on topics to talk to dean",
+        "Build a public profile (will discuss later)",
+        "Black Lives Matter initiative: Connect with CMU SPIRIT.",
+        "Update current position holder names from constitution to website.",
+      ],
+      "Tasks for next meeting": [
+        "Discuss the findings of Black Lives Matter initiative",
+      ],
+    },
+  },
+  {
+    "meeting-date": {
+      date: "6",
+      month: "Jun",
+      year: "2020",
+    },
+    minutes: {
+      main: [
+        "Discussed what to talk to the dean about",
+        "Members appointed to the organizational roles",
+        "Discussed diversity and inclusiveness of the scs students we gather data from and national issues affecting them ",
+        "Updated members list",
+      ],
+      "Tasks for next meeting": ["Think about the two fronts of diversity"],
+    },
+  },
+];
--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
-$(() => {});
-
-$("#meeting-detailed-close").click(() => {
-  toggleMeetingsDetails(false);
-});
-
-$(".calender-date-item").click(() => {
-  toggleMeetingsDetails(true);
+$(() => {
+  setupMinutes(minutes);
 });
 
 const toggleMeetingsDetails = (val = true) => {
@@ -16,4 +10,89 @@ const toggleMeetingsDetails = (val = true) => {
     $("#meetings-calendar").removeClass("hidden");
     $("#meeting-detailed").addClass("hidden");
   }
+};
+
+const createCalenderDiv = (meetingDate, key) => {
+  return `<div class="calender-date-item" data-id="${key}">
+            <div class="date">${meetingDate.date}</div>
+            <div class="month">${meetingDate.month}</div>
+            <div class="line"></div>
+        </div>`;
+};
+
+const handleShowMinutes = (event, json) => {
+  let target = $(event.currentTarget);
+  let id = target.data("id");
+  let minutes = json[id];
+
+  // Setup date
+  let meetingDate = minutes["meeting-date"];
+
+  $("#md-date").html(meetingDate.date);
+  $("#md-month").html(meetingDate.month);
+  $("#md-year").html(meetingDate.year);
+
+  setupMinutesDetails(minutes["minutes"]);
+  toggleMeetingsDetails(true);
+};
+
+const convertNotesJsonToHtml = (json) => {
+  let notes = "";
+
+  if (Array.isArray(json)) {
+    json.map((val) => {
+      notes += convertNotesJsonToHtml(val);
+    });
+  } else if (typeof json === "object") {
+    for (const key in json) {
+      notes += `<li>${key}</li>`;
+      notes += `<ul>`;
+      notes += convertNotesJsonToHtml(json[key]);
+      notes += `</ul>`;
+    }
+  } else if (typeof json === "string") {
+    return `<li>${json}</li>`;
+  }
+
+  return notes;
+};
+
+const setupMinutesDetails = (minutes) => {
+  // Initialise
+  $("#md-notes").html(`<ul id="md-notes-main"></ul>`);
+
+  $("#md-notes-main").html(convertNotesJsonToHtml(minutes["main"]));
+
+  let restOfTheNotes = [];
+
+  for (const key in minutes) {
+    if (key == "main") continue;
+
+    // Add header
+    restOfTheNotes += `<div class="note-type">${key}</div>`;
+
+    // notes
+    restOfTheNotes += `<ul>`;
+    restOfTheNotes += convertNotesJsonToHtml(minutes[key]);
+    restOfTheNotes += `</ul>`;
+  }
+
+  $("#md-notes").append(restOfTheNotes);
+};
+
+const setupMinutes = (json) => {
+  let calenderDivs = "";
+  json.forEach((minutes, key) => {
+    calenderDivs += createCalenderDiv(minutes["meeting-date"], key);
+  });
+
+  // Add calender values
+  $("#meetings-calendar").html(calenderDivs);
+
+  // Setup actions
+  $("#meeting-detailed-close").click(() => {
+    toggleMeetingsDetails(false);
+  });
+
+  $(".calender-date-item").click((event) => handleShowMinutes(event, json));
 };
